0db73fd0ea8f9312e0ed9f8be20ca000f1e599f1
MUHAMMED FATİH YEŞİLDAL <200527108@stu.istinye.edu.tr>
2023-10-20 16:57:56 +0300

    Layout partials occured

---
+++ LibProject/Controllers/layoutPartialController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibProject.Controllers
+{
+    public class layoutPartialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult scripts()
+        {
+            return PartialView();
+        }
+        public PartialViewResult head()
+        {
+            return PartialView();
+        }
+        public PartialViewResult navbar()
+        {
+            return PartialView();
+        }
+    }
+}
