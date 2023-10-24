37e6c2b0c3922eef35a88af7f467fdacaeafca07
MUHAMMED FATİH YEŞİLDAL <200527108@stu.istinye.edu.tr>
2023-10-25 00:39:52 +0300

    book controller insert codes

---
+++ LibProject/Controllers/bookController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.abstracts;
+using EntityLayer.concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibProject.Controllers
+{
+    public class bookController : Controller
+    {
+        private readonly IBookService _bookService;
+
+        public bookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _bookService.getAll();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult addBook()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult addBook(book p)
+        {
+            _bookService.insert(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
